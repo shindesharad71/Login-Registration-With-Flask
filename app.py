3c1e06beabd6cda7ce4eeeacf60f502919d6787a
Sharad Shinde <shindesharad71@gmail.com>
2018-09-03 23:24:28 +0530

    login screen updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response, jsonify, request
+from flask import Flask, render_template, request, Blueprint, flash, g, redirect, session, url_for
 import db
 import jwt
 import config
@@ -7,11 +7,30 @@ app = Flask(__name__)
 
 cursor, conn = db.connection(app)
 
-@app.route('/')
+@app.route('/login', methods=('GET', 'POST'))
 def index():
-    cursor.execute('SELECT * FROM auth')
-    users = cursor.fetchall()
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        error = None
+        user = cursor.execute('SELECT * FROM auth WHERE email = ?', (email)).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not user['password'] == password:
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('/home'))
+
+        flash(error)
     return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.debug = config.debug
